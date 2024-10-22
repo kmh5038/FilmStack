c14c961bdc991286c34dd1ecb99ea2a9fbf8ae0a
kmh5038 <kmh5038@gmail.com>
2024-10-22 18:14:11 +0900

    feat: MovieOnPlayView UI 수정 및 MovieSearchView추가
    - MovieOnPlayView 배경색 변경 및
    - 탭뷰 배경색 변경
    - MovieSearchView 기본뷰 구현

--- FilmStack/FilmStack/View/ContentView.swift
+++ FilmStack/FilmStack/View/ContentView.swift
@@ -11,12 +11,33 @@ struct ContentView: View {
     @StateObject private var movieOnPlayViewModel = MovieOnPlayViewModel()
     
     var body: some View {
-        TabView {
-            MovieOnPlayView(viewModel: movieOnPlayViewModel)
-                .tabItem {
-                    Image(systemName: "1.square.fill")
-                    Text("상영중 영화")
-                }
+        ZStack {
+            TabView {
+                MovieOnPlayView(viewModel: movieOnPlayViewModel)
+                    .tabItem {
+                        Image(systemName: "film")
+                        Text("상영중 영화")
+                    }
+                
+                MovieSearchView()
+                    .tabItem {
+                        Image(systemName: "magnifyingglass")
+                        Text("영화 검색")
+                    }
+            }
+            .tint(.white)
+            .onAppear {
+                let appearance = UITabBarAppearance()
+                appearance.configureWithOpaqueBackground()
+                appearance.backgroundColor = .black
+                
+                // 탭바 상단에 회색 경계선 추가
+                appearance.shadowImage = UIImage()
+                appearance.shadowColor = .systemGray6
+                
+                UITabBar.appearance().standardAppearance = appearance
+                UITabBar.appearance().scrollEdgeAppearance = appearance
+            }
         }
     }
 }
--- FilmStack/FilmStack/View/MovieOnPlayView/MovieListView.swift
+++ FilmStack/FilmStack/View/MovieOnPlayView/MovieListView.swift
@@ -12,38 +12,53 @@ struct MovieListView: View {
     let movies: [MovieInfoModel]
     
     var body: some View {
-        List {
-            ForEach(movies, id: \.self) { movie in
-                HStack() {
-                    Text(movie.boxOfficeInfo.rank ?? "")
-                        .font(.system(size: 20, weight: .semibold))
-                        .frame(width: 24, alignment: .leading)
-                    
-                    VStack(alignment: .leading) {
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            
+            List {
+                ForEach(movies, id: \.self) { movie in
+                    HStack {
+                        Text(movie.boxOfficeInfo.rank ?? "")
+                            .foregroundColor(.white)
+                            .font(.system(size: 20, weight: .semibold))
+                            .frame(width: 24, alignment: .leading)
+                        
+                        
+                        
                         if let url = movie.posterURL {
                             AsyncImage(url: url) { image in
-                                image.image?.resizable()
+                                image.resizable()
+                            } placeholder: {
+                                Color.gray
                             }
                             .frame(width: 80, height: 100)
                             .cornerRadius(12)
                         }
+                        
+                        VStack(alignment: .leading) {
+                            Text(movie.boxOfficeInfo.movieNm ?? "")
+                                .foregroundColor(.white)
+                                .font(.system(size: 16, weight: .semibold))
+                                .padding(.bottom, 10)
+                            Text("개봉일자: \(movie.boxOfficeInfo.openDt ?? "")")
+                                .foregroundColor(.white)
+                                .font(.system(size: 12, weight: .semibold))
+                            Text("누적관객수: \(NumberFormatter.formatNumber(movie.boxOfficeInfo.audiAcc ?? "")) 명")
+                                .foregroundColor(.white)
+                                .font(.system(size: 12, weight: .semibold))
+                            Text("상영시간: \(movie.runtime ?? "") 분")
+                                .foregroundColor(.white)
+                                .font(.system(size: 12, weight: .semibold))
+                        }
+                        .padding(.horizontal, 10)
                     }
+                    .listRowBackground(Color.black)
                     
-                    VStack(alignment: .leading) {
-                        Text(movie.boxOfficeInfo.movieNm ?? "")
-                            .font(.system(size: 16, weight: .semibold))
-                            .padding(.bottom, 10)
-                        Text("개봉일자: \(movie.boxOfficeInfo.openDt ?? "")")
-                            .font(.system(size: 12, weight: .semibold))
-                        Text("누적관객수: \(NumberFormatter.formatNumber(movie.boxOfficeInfo.audiAcc ?? "")) 명")
-                            .font(.system(size: 12, weight: .semibold))
-                        Text("상영시간: \(movie.runtime ?? "") 분")
-                            .font(.system(size: 12, weight: .semibold))
-                    }
-                    .padding(.horizontal, 10)
                 }
             }
+            .listStyle(.plain)
+            .background(Color.black)  // List의 배경색을 검정색으로 설정
         }
-        .listStyle(.plain)
     }
 }
+
